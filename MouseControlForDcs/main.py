09f8c0603109aee1b5b8121e0ddcf0e14992c254
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2023-11-03 21:59:36 -0700

    Update main.py

--- MouseControlForDcs/main.py
+++ MouseControlForDcs/main.py
@@ -4,18 +4,20 @@ from PySide2 import QtCore,QtWidgets
 from ui_mouseControl import Ui_MainWindow
 
 
+gamepad = vg.VX360Gamepad()
+gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+gamepad.update()
+time.sleep(0.5)
+
 def convert(sens, key):
-    gamepad = vg.VX360Gamepad()
-    gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
-    gamepad.update()
-    time.sleep(0.5)
 
     offset_x, offset_y = pyautogui.position()
     while True:
         x, y = pyautogui.position()
         x = max(-1, min(1, (x-offset_x)/100*float(sens)))
         y = max(-1, min(1, (y-offset_y)/100*float(sens)))
-        gamepad.right_joystick_float(x_value_float=x, y_value_float=y)
+        print(x,y)
+        gamepad.right_joystick_float(x_value_float=x, y_value_float=-y)
         gamepad.update()
         time.sleep(0.01)
         if stop_thread:
