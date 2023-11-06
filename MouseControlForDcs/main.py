3104d6bf3054837872f08a878d30cec985104cf1
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2023-11-05 17:52:16 -0800

    fixed thread not stopping

--- MouseControlForDcs/main.py
+++ MouseControlForDcs/main.py
@@ -3,20 +3,16 @@ import vgamepad as vg
 from PySide2 import QtCore,QtWidgets
 from ui_mouseControl import Ui_MainWindow
 
-
 gamepad = vg.VX360Gamepad()
 gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
 gamepad.update()
-time.sleep(0.5)
 
 def convert(sens, key):
-
     offset_x, offset_y = pyautogui.position()
     while True:
         x, y = pyautogui.position()
         x = max(-1, min(1, (x-offset_x)/100*float(sens)))
         y = max(-1, min(1, (y-offset_y)/100*float(sens)))
-        print(x,y)
         gamepad.right_joystick_float(x_value_float=x, y_value_float=-y)
         gamepad.update()
         time.sleep(0.01)
@@ -25,38 +21,27 @@ def convert(sens, key):
         if keyboard.is_pressed(key):
             offset_x, offset_y = pyautogui.position()
 
-stop_thread = False
-
 class main(QtWidgets.QMainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-       
+        self.ui.setupUi(self)  
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-
         self.show()
         self.setWindowTitle('Mouse Control For Dcs')
-
-        self.ui.startBtn.clicked.connect(self.start)
+        self.ui.startBtn.clicked.connect(self.start)       
         
     def start(self):
-        if self.ui.startBtn.text() == 'Stop':
+        global stop_thread 
+        if self.ui.startBtn.text() == 'Stop':      
             stop_thread = True
             self.ui.startBtn.setText('Start')
         else:
             self.ui.startBtn.setText('Stop')
             stop_thread = False
-            threading.Thread(target=convert, daemon=True, args=(self.ui.sens.text(),self.ui.key.text(),)).start()
-            
-    def close(self):
-        quit()
+            threading.Thread(target=convert, args=(self.ui.sens.text(),self.ui.key.text(),)).start()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = main()
     sys.exit(app.exec_())
-
-
-
-
